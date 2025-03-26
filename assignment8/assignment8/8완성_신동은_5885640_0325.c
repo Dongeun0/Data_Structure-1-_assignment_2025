b90f1a7b0bb5ceffafccb223eef1af9d22294a42
Dongeun0 <sdegood@naver.com>
2025-03-26 18:46:45 +0900

    마지막커밋

--- assignment8/assignment8/8완성_신동은_5885640_0325.c
+++ assignment8/assignment8/8완성_신동은_5885640_0325.c
@@ -31,10 +31,12 @@ End main function
 #include <stdio.h>
 
 int main() {
+	//각 첫번째 배열과 두번째 배열 저장
 	int Arr[2][2];
 	int arr[2][2];
 
 	printf("첫 번째 2x2 행렬 입력: \n");
+	//입력한 정수를 배열의 각 위치에 저장
 	for (int i = 0; i < 2; i++) {
 		for (int j = 0; j < 2; j++) {
 			scanf_s("%2d", &Arr[i][j]);
@@ -42,6 +44,7 @@ int main() {
 	}
 
 	printf("두 번째 2x2 행렬 입력: \n");
+	//입력한 정수를 배열의 각 위치에 저장
 	for (int i = 0; i < 2; i++) {
 		for (int j = 0; j < 2; j++) {
 			scanf_s("%2d", &arr[i][j]);
@@ -49,7 +52,9 @@ int main() {
 	}
 
 	printf("행렬 덧셈 결과: \n");
+	//두 배열에 위치에 있는 정수들의 합을 계산하고 위치에 맞게 다시 출력함
 	for (int i = 0; i < 2; i++) {
+		//각 위치마다 두 배열에 같은 자리에 있는 정수들을 더하고 출력
 		for (int j = 0; j < 2; j++) {
 			int sum = Arr[i][j] + arr[i][j];
 			printf("%2d", sum);
--- assignment9/assignment9/9완성_신동은_5885640_0325.c
+++ assignment9/assignment9/9완성_신동은_5885640_0325.c
@@ -1,33 +1,96 @@
+/*
+Function Main start
+	Get a
+	Print "정수의 개수를 입력하세요: "
+	Scanf a
+
+	arr = 동적 할당
+
+	Print " %d개의 정수를 입력하세요: \n", a
+
+	If (arr = NULL) then
+		Print "메모리 할당 실패"
+		Return 1
+	End if
+
+	For (i = 0; i < a; i++) do
+		Scanf arr[i]
+	End for
+
+	For (i = 0; i < a-1; i++) do
+		min = i
+		For (j = i + 1; j < a; j++) do
+			If (arr[j] < arr[min]) then
+				min = j
+			End if
+		End for
+
+		If (min != i) then
+			temp = arr[i]
+			arr[i] = arr[min]
+			arr[min] = temp
+		End if
+	End for
+
+	Print "오름차순으로 정렬된 정수들: "
+	For (i = 0; i < a; i++) do
+		Print"%d ", arr[i];
+	End For
+
+	free arr
+End Main
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-	int a, temp;
+	int a;
 
 	printf("정수의 개수를 입력하세요: ");
 	scanf_s("%d", &a);
+
+	//개수 저장할 동적 메모리 할당
 	int* arr = malloc(sizeof(int)*a);
-	if (arr != NULL) {
-		printf("%d개의 정수를 입력하세요: \n", a);
-		for (int i = 0; i < a; i++) {
-			scanf_s("%d ", arr + i);
-		}
-		scanf_s("%[^\n]s", a);
-		fflush(stdin);
-		printf("오름차순으로 정렬된 정수들: ");
-		for (int i = 0; i < a; i++) {
-			for (int j = i + 1;j < a; j++) {
-				if (arr[j] < arr[i]) {
-					temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
+
+	printf("%d개의 정수를 입력하세요: \n", a);
+
+	//메모리 할당 실패하면 에러 안내 후 종료
+	if (arr == NULL) {
+		printf("메모리 할당 실패\n");
+		return 1;
+	}
+
+	//a개의 정수를 입력 받음
+	for (int i = 0; i < a; i++) {
+		scanf_s("%d ", &arr[i]);
+	}
+
+	for (int i = 0; i < a-1; i++) {
+		//i를 최소값으로 초기화
+		int min = i;
+		//i번째 이후 검사로 최소값을 찾음
+		for (int j = i + 1; j < a; j++) {
+			//만약 더 작은 최소값을 찾으면 그 값으로 변경
+			if (arr[j] < arr[min]) {
+				min = j;
 			}
 		}
-		for (int i = 0; i < a; i++) {
-			printf("%d ", arr[i]);
+		//최소값이 현재값과 다르면 교환
+		if (min != i) {
+			int temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
 		}
-		free(a);
 	}
+
+	//정렬된 정수 출력
+	printf("오름차순으로 정렬된 정수들: ");
+	for (int i = 0; i < a; i++) {
+		printf("%d ", arr[i]);
+	}
+
+	//메모리 해제
+	free(arr);
 	return 0;
 }
